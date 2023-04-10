bd94f1f8b0852a266104d899de8d016595bbc621
lms <lms19880914@qq.com>
2023-04-10 19:45:54 +0800

    文案错误修正

--- viewer/src/pages/Index.tsx
+++ viewer/src/pages/Index.tsx
@@ -21,7 +21,7 @@ const Basic: FC = () => {
               <h3>
                 <b style={{color: "#e95420"}}>Step1: Upload Samples</b><br/>
                 1. Download the sample Excel template.   <a href={`${url}/static/SampleTemplate.xlsx`}>Click to Download Template</a>;<br/>
-                2. The experimenter simply fills in the SampleID of each sample and the corresponding grouping, PlateDesign supports up to three different grouping dimensions;<br/>
+                2. The experimenter simply fills in the SampleID of each sample and the corresponding grouping, InjectionDesign supports up to three different grouping dimensions;<br/>
                 3. Users must ensure that this SampleID is unique within the same Excel;<br/>
                 4. After successful upload, the system will automatically create a corresponding project. Users can edit it again and again after uploading, without having to upload it again;<br/>
               </h3>
@@ -29,9 +29,8 @@ const Basic: FC = () => {
                 <b style={{color: "#e95420"}}>Step2: Design Params </b><br/>
                 1. <b>Plate</b>: Choose the plate specification.(91-well plate, 96-well plate and 384-well plate);<br/>
                 2. <b>Max Samples on Single Plate</b>: The maximum number of common samples carried on each plate; Total number of wells in the plate = MaxSamplesOnSinglePlate + QC Samples;<br/>
-                3. <b>Plate Number</b>: yAxis format of the plate. Both numeric and English formats are supported;<br/>
-                4. <b>Direction</b>: The orientation of the sample layout;<br/>
-                5. <b>QC Type</b>: The system currently supports 5 QC types: Blank, Long-Term Reference, Pooled, Solvent and Custom QC Samples;<br/>
+                3. <b>Direction</b>: The orientation of the sample layout;<br/>
+                4. <b>QC Type</b>: The system currently supports 5 QC types: Blank, Long-Term Reference, Pooled, Solvent and Custom QC Samples;<br/>
 
                 Users can simply select multiple empty spaces on the well-plate (supports multi-selection mode with Ctrl or Shift key). Users can also set the position directly by dragging and dropping the relevant position with the mouse.
               </h3>
